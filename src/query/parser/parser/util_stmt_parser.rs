50477e90e4abaea9c26dd36245c58fe0c334eedd
Admin <3353074868@qq.com>
2026-02-19 10:03:48 +0800

    pass all tests

--- src/query/parser/parser/util_stmt_parser.rs
+++ src/query/parser/parser/util_stmt_parser.rs
@@ -292,13 +292,14 @@ impl UtilStmtParser {
     /// 解析 YIELD 语句
     pub fn parse_yield_statement(&mut self, ctx: &mut ParseContext) -> Result<Stmt, ParseError> {
         let start_span = ctx.current_span();
+        ctx.expect_token(TokenKind::Yield)?;
 
         let yield_clause = ClauseParser::new().parse_yield_clause(ctx)?;
 
         Ok(Stmt::Yield(YieldStmt {
             span: start_span,
             items: yield_clause.items,
-            where_clause: None,
+            where_clause: yield_clause.where_clause,
             distinct: false,
         }))
     }
